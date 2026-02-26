d1f588f7b613597ad7218b4af588eec4d1f1e867
abir-nath <abirnath@sigilotech.com>
2026-02-26 15:01:29 +0530

    Create Controller and Views and Update Model for Asset

--- Models/Asset.cs
+++ Models/Asset.cs
@@ -6,21 +6,23 @@ public class Asset
 {
     public int Id { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Asset Name is required")]
     [StringLength(100)]
     public string AssetName { get; set; }
 
-    [Required]
-    public string AssetType { get; set; }
-
-    [Required]
+    [Required(ErrorMessage = "Serial Number is required")]
+    [StringLength(50)]
     public string SerialNumber { get; set; }
 
+    [Required(ErrorMessage = "Asset Type is required")]
+    public string AssetType { get; set; }
+
+    [Required(ErrorMessage = "Purchase Date is required")]
     [DataType(DataType.Date)]
     public DateTime PurchaseDate { get; set; }
 
     public bool IsAvailable { get; set; } = true;
 
-    // Navigation property
+    public bool IsDeleted { get; set; } = false;
     public ICollection<EmployeeAsset>? EmployeeAssets { get; set; }
 }
