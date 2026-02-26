55217ea8c723f05d464249b58720b2442266f52c
abir-nath <abirnath@sigilotech.com>
2026-02-26 11:20:55 +0530

    Add Controller

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeAssetManagementSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAssetManagementSystem.Controllers;
+
+public class EmployeesController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public EmployeesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+}
